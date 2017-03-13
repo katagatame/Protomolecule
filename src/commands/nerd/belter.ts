0d9c021bd23bc1a062a8b89b863edabaed8b4c58
katagatame <nathan18t@gmail.com>
2017-03-13 06:08:27 -0400

    Taking a step back

--- src/commands/nerd/belter.ts
+++ src/commands/nerd/belter.ts
@@ -54,31 +54,6 @@ export default class Dictionary extends Command<Bot>
             return message.channel.sendEmbed(embed, '', { disableEveryone: true });
         }
 
-        if (results.length >= 2)
-        {
-            if (Term.isSpecificTerm(results, args.join(' ')))
-            {
-               let result: Term = results[0].original;
-
-                // build the output embed
-                const embed: RichEmbed = new RichEmbed()
-                    .setColor(0x274E13)
-                    .setAuthor('Disekowtalowda Dictionary', message.guild.iconURL)
-                    .addField(result.term, '*' + result.pronounciation + '*',  true)
-                    .addField('Part of Speech', result.partOfSpeech, true)
-                    .addField('\u200b', '\u200b', true)
-                    .addField('Definition', result.definition, true)
-                    .addField('Usage', result.usage, true)
-                    .addField('\u200b', '\u200b', true)
-                    .addField('Etymology', result.etymology, false)
-                    .setTimestamp();
-
-                return message.channel.sendEmbed(embed, '', { disableEveryone: true });
-            }
-            else
-            {
-                return message.channel.sendMessage(`More than one term found: \`${results.map((el: any) => {return el.original.term}).join(', ')}\`,  please be more specific.`);}
-            }
-        }
+        console.log(results);
     }
 }
