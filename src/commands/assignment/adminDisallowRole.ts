f8506bfc636569296eeccca9d28c439bc8321d0c
nathan <nathan18T@gmail.com>
2017-03-22 15:24:24 -0400

    Multiple Roles
    - allow for multiple roles

--- src/commands/assignment/adminDisallowRole.ts
+++ src/commands/assignment/adminDisallowRole.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import { Bot, Command } from 'yamdbf';
-import { GuildMember, Message, Role, User } from 'discord.js';
+import { Message, RichEmbed, Role, User } from 'discord.js';
 import * as fuzzy from 'fuzzy';
 import Assignment from '../../util/assignment';
 import Constants from '../../util/constants';
@@ -14,7 +14,7 @@ export default class DisallowRole extends Command<Bot>
             name: 'disallow',
             aliases: ['d'],
             description: 'Disallow Role',
-            usage: '<prefix>disallow <Role Name>',
+            usage: '<prefix>disallow <Role Name>, <prefix>d <Role Name>',
             extraHelp: 'Use this command to disallow roles to be self-assignable.',
             group: 'assignment',
             roles: ['The Rocinante'],
@@ -25,19 +25,16 @@ export default class DisallowRole extends Command<Bot>
     public action(message: Message, args: string[]): Promise<any>
     {
         // variable declaration
-        
         const guildStorage: any = this.bot.guildStorages.get(message.guild);
         let availableRoles: Array<any> = guildStorage.getItem('Server Roles');        
         let adminCommandRole: Role = message.guild.roles.find('name', 'The Rocinante');
         let scrub: Boolean = false;
-        let roleArg: string = '';
+        let roleArgs: Array<any> = new Array();
         let role: Role;        
 
         // make sure a role was specified
-        if (Constants.disallowRegExp.test(message.content))
-            roleArg = Constants.disallowRegExp.exec(message.content)[1];
-        else
-            return message.channel.sendMessage('Please specify a role to disallow.');
+        if (args.length === 0)
+            return message.channel.sendMessage('Please specify a role to allow.');
         
         if (Constants.scrubRegExp.test(message.content))
             scrub = true;
@@ -45,42 +42,29 @@ export default class DisallowRole extends Command<Bot>
         // make sure available roles isn't empty
         if (availableRoles === null)
             return message.channel.sendMessage(`No roles currently allowed.`);
-        
-        // search for role
-        let options: any = { extract: (el: any) => { return el.name } };
-        let results: any = fuzzy.filter(roleArg, availableRoles, options);
 
-        // check if role is valid
-        if (results.length === 0)
-            return message.channel.sendMessage(`\`${roleArg}\` is not a valid role.`);
-        
-        // disallow role
-        if (results.length === 1)
+        // create array from user input
+        roleArgs = args.filter((el: string) => { return (!Constants.scrubRegExp.test(el)) ? true : false; }).map((el: string) => { return el.replace('\,', '') });
+
+        // if one role specified
+        if (roleArgs.length === 1)
         {
-            // role from result
-            role = message.guild.roles.get(results[0].original.id);
+            // search for role
+            let options: any = { extract: (el: any) => { return el.name } };
+            let results: any = fuzzy.filter(roleArgs[0], availableRoles, options);
+
+            // check if role is valid
+            if (results.length === 0)
+                return message.channel.sendMessage(`\`${roleArgs[0]}\` is not a valid role.`);
             
-            // remove the role from the allowed list
-            availableRoles.splice(Assignment.getRoleToRemove(availableRoles, role.name), 1);
-            guildStorage.setItem('Server Roles', availableRoles);
-
-            // check if scrub option was used
-            if (scrub)
-                // remove role from all users
-                Assignment.removeRoleFromUserBase(message, role);
-            else
-                // display success message
-                return message.channel.sendMessage(`\`${role.name}\` successfully disallowed.`);
-        }
-
-        // more than one role found
-        if (results.length > 1)
-        {
-            // check if roleArg is specifically typed
-            if (Assignment.isSpecificResult(results, roleArg))
+            // disallow role
+            if (results.length === 1)
             {
+                // role from result
+                role = message.guild.roles.get(results[0].original.id);
+                
                 // remove the role from the allowed list
-                availableRoles.splice(Assignment.getRoleToRemove(availableRoles, Assignment.getSpecificRoleName(results, roleArg)), 1);
+                availableRoles.splice(Assignment.getRoleToRemove(availableRoles, role.name), 1);
                 guildStorage.setItem('Server Roles', availableRoles);
 
                 // check if scrub option was used
@@ -90,9 +74,95 @@ export default class DisallowRole extends Command<Bot>
                     // display success message
                     return message.channel.sendMessage(`\`${role.name}\` successfully disallowed.`);
             }
-            else
-                // be more specific
-                return message.channel.sendMessage(`More than one role found: \`${results.map((el: any) => { return el.string; }).join(', ')}\`,  please be more specific.`);
-        }            
+
+            // more than one role found
+            if (results.length > 1)
+            {
+                // check if roleArg is specifically typed
+                if (Assignment.isSpecificResult(results, roleArgs[0]))
+                {
+                    // remove the role from the allowed list
+                    availableRoles.splice(Assignment.getRoleToRemove(availableRoles, Assignment.getSpecificRoleName(results, roleArgs[0])), 1);
+                    guildStorage.setItem('Server Roles', availableRoles);
+
+                    // check if scrub option was used
+                    if (scrub)
+                        Assignment.removeRoleFromUserBase(message, role);
+                    else
+                        // display success message
+                        return message.channel.sendMessage(`\`${role.name}\` successfully disallowed.`);
+                }
+                else
+                    // be more specific
+                    return message.channel.sendMessage(`More than one role found: \`${results.map((el: any) => { return el.string; }).join(', ')}\`,  please be more specific.`);
+            }
+        }
+
+        // if more than one role specified
+        if (roleArgs.length > 1)
+        {
+            // variable declaration
+            let invalidRoles: Array<string> = new Array();
+            let validRoles: Array<Role> = new Array();
+            const embed: RichEmbed = new RichEmbed();
+
+            roleArgs.forEach((el: string) => {
+                // search for role
+                let options: any = { extract: (el: any) => { return el.name } };
+                let results: any = fuzzy.filter(el, availableRoles, options);
+
+                 // check if role is valid
+                if (results.length === 0)
+                    invalidRoles.push(el);
+                
+                // if one role found
+                if (results.length === 1)
+                {
+                    // role from result
+                    role = message.guild.roles.get(results[0].original.id);
+
+                    // remove the role from the allowed list
+                    availableRoles.splice(Assignment.getRoleToRemove(availableRoles, role.name), 1);
+
+                    // add role to valid array
+                    validRoles.push(role);
+                }
+
+                // more than one role found
+                if (results.length > 1)
+                {
+                    // check if roleArg is specifically typed
+                    if (Assignment.isSpecificResult(results, el))
+                    {
+                        // role from roleArg
+                        role = Assignment.getSpecificRole(results, el);
+
+                        // remove the role from the allowed list
+                        availableRoles.splice(Assignment.getRoleToRemove(availableRoles, Assignment.getSpecificRoleName(results, el)), 1);
+
+                        // add role to valid array
+                        validRoles.push(role);
+                    }
+                    else
+                        // add inspecific results to invalid array
+                        results.forEach((el: any) => { invalidRoles.push(el.string); });
+                }
+            });
+
+            // update roles
+            guildStorage.setItem('Server Roles', availableRoles);
+
+            // build output embed
+            embed
+                .setColor(0x206694)
+                .setTitle(message.guild.name + ': Roles Update')
+                .addField('Disllowed Roles', validRoles.join('\n') ? validRoles.join('\n') : '\u200b', true)
+                .addField('Invalid Roles', invalidRoles.join('\n') ? invalidRoles.join('\n') : '\u200b', true)
+                .setDescription('Invalid Roles are either already disallowed, incorrectly typed, or not a current server role.')
+                .setTimestamp();
+            
+            // display output embed
+            return message.channel.sendEmbed(embed, '', { disableEveryone: true });
+        }
     }
-}
+};
--- src/commands/assignment/sharedDestroyRole.ts
+++ src/commands/assignment/sharedDestroyRole.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import { Bot, Command } from 'yamdbf';
-import { GuildMember, Message, Role, User } from 'discord.js';
+import { GuildMember, Message, RichEmbed, Role, User } from 'discord.js';
 import * as fuzzy from 'fuzzy';
 import Assignment from '../../util/assignment';
 import Constants from '../../util/constants';
@@ -12,10 +12,9 @@ export default class DestroyRole extends Command<Bot>
     {
         super(bot, {
             name: 'dr',
-            aliases: ['DR', 'dR', 'Dr'],
             description: 'Destroy Role',
             usage: '<prefix>dr <Role Name>',
-            extraHelp: 'Use this command to remove a specific role.',
+            extraHelp: 'Remove one of the specified self-assignable roles.',
             group: 'assignment',
             guildOnly: true
         });
@@ -26,59 +25,121 @@ export default class DestroyRole extends Command<Bot>
         // variable declaration
         const guildStorage: any = this.bot.guildStorages.get(message.guild);
         let availableRoles: Array<any> = guildStorage.getItem('Server Roles');
-        let roleArg: string = '';
+        let roleArgs: Array<any> = new Array();
         let role: Role;
 
-        // make sure a role was specified
-        if (Constants.destroyRegExp.test(message.content))
-            roleArg = Constants.destroyRegExp.exec(message.content)[1];
-        else
-            return message.channel.sendMessage('Please specify a role to remove.');
-
         // make sure there are allowed roles
-        if (availableRoles === null)
+        if (availableRoles === null || availableRoles.length === 0)
             return message.channel.sendMessage('There are currently no self-assignable roles.');
-        
-        // search for role
-        let options: any = { extract: (el: any) => { return el.name } };
-        let results: any = fuzzy.filter(roleArg, availableRoles, options);
 
-        // check if role is valid
-        if (results.length === 0)
-            return message.channel.sendMessage(`\`${roleArg}\` is not a valid role.`);
-        
-        // remove role
-        if (results.length === 1)
-        {
-            // try to find user
-            message.guild.fetchMember(message.author.id).then((user: GuildMember) => {
-                user.removeRole(results[0].original.id);
-                return message.channel.sendMessage(`\`${results[0].original.name}\` successfully removed.`);
-            }).catch((err: any) => {
-                return message.channel.sendMessage(`User could not be found.`);
-            });            
-        }
+        // make sure a role was specified
+        if (args.length === 0)
+            return message.channel.sendMessage('Please specify a role to assign.');
 
-        // more than one role found
-        if (results.length > 1)
+        // create array from user input
+        roleArgs = args.map((el: any) => { return el.replace(',', ''); });
+        
+        // if one role specified
+        if (roleArgs.length === 1)
         {
-            // check if roleArg is specifically typed
-            if (Assignment.isSpecificResult(results, roleArg))
+            // search for role
+            let options: any = { extract: (el: any) => { return el.name } };
+            let results: any = fuzzy.filter(roleArgs[0], availableRoles, options);
+
+            // check if role is valid
+            if (results.length === 0)
+                return message.channel.sendMessage(`\`${roleArgs[0]}\` is not a valid role.`);
+            
+            // remove role
+            if (results.length === 1)
             {
-                // grab the role to remove
-                role = message.guild.roles.find('name', Assignment.getSpecificRoleName(results, roleArg));
-                
                 // try to find user
                 message.guild.fetchMember(message.author.id).then((user: GuildMember) => {
-                    user.removeRole(role);
-                    return message.channel.sendMessage(`\`${role.name}\` successfully removed.`);
+                    user.removeRole(results[0].original.id);
+                    return message.channel.sendMessage(`\`${results[0].original.name}\` successfully removed.`);
                 }).catch((err: any) => {
                     return message.channel.sendMessage(`User could not be found.`);
                 });
             }
-            else
-                // be more specific
-                return message.channel.sendMessage(`More than one role found: \`${results.map((elem: any) => {return elem.string}).join(', ')}\`,  please be more specific.`);
+
+            // more than one role found
+            if (results.length > 1)
+            {
+                // check if roleArg is specifically typed
+                if (Assignment.isSpecificResult(results, roleArgs[0]))
+                {
+                    // grab the role to remove
+                    role = message.guild.roles.find('name', Assignment.getSpecificRoleName(results, roleArgs[0]));
+
+                    // try to find user
+                    message.guild.fetchMember(message.author.id).then((user: GuildMember) => {
+                        user.removeRole(role);
+                        return message.channel.sendMessage(`\`${role.name}\` successfully removed.`);
+                    }).catch((err: any) => {
+                        return message.channel.sendMessage(`User could not be found.`);
+                    });
+                }
+                else
+                    // be more specific
+                    return message.channel.sendMessage(`More than one role found: \`${results.map((elem: any) => {return elem.string}).join(', ')}\`,  please be more specific.`);
+            }
+        }
+
+        // if more than one role specified
+        if (roleArgs.length > 1)
+        {
+            // variable declaration
+            let invalidRoles: Array<string> = new Array();
+            let validRoles: Array<Role> = new Array();
+            const embed: RichEmbed = new RichEmbed();
+
+            roleArgs.forEach((el: any) => {
+                // search for role
+                let options: any = { extract: (el: any) => { return el.name } };
+                let results: any = fuzzy.filter(el, availableRoles, options);
+
+                // check if role is valid
+                if (results.length === 0)
+                    invalidRoles.push(el);
+                
+                // assign role
+                if (results.length === 1)
+                {
+                    // role from result
+                    role = message.guild.roles.get(results[0].original.id);
+                    validRoles.push(role);
+                }
+
+                // more than one role found
+                if (results.length > 1)
+                {
+                    // check if roleArg is specifically typed
+                    if (Assignment.isSpecificResult(results, el))
+                    {
+                        // grab the role to be assigned
+                        role = message.guild.roles.find('name', Assignment.getSpecificRoleName(results, el));
+                        validRoles.push(role);
+                    }
+                    else
+                        // be more specific
+                        return message.channel.sendMessage(`More than one role found: \`${results.map((el: any) => {return el.string}).join(', ')}\`,  please be more specific.`);
+                }
+            });
+
+            // remove roles
+            validRoles.forEach((el: Role) => { message.guild.member(message.author.id).removeRole(el); });
+
+             // build output embed
+            embed
+                .setColor(0x206694)
+                .setTitle(message.guild.name + ': Roles Update')
+                .addField('Removed Roles', validRoles.join('\n') ? validRoles.join('\n') : '\u200b', true)
+                .addField('Invalid Roles', invalidRoles.join('\n') ? invalidRoles.join('\n') : '\u200b', true)
+                .setDescription('Invalid Roles are either already allowed, incorrectly typed, or not a current server role.')
+                .setTimestamp();
+            
+            // display output embed
+            return message.channel.sendEmbed(embed, '', { disableEveryone: true });
         }
     }
-}
+};
